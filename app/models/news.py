f953e57c487d976265477a7a062e95cbc55494bc
Francis Kinyae <fmkinyae@gmail.com>
2021-08-07 20:09:04 +0300

    add objects for the news sources

--- app/models/news.py
+++ app/models/news.py
@@ -3,11 +3,9 @@ class News:
     News class that defines the news objects
     '''
     
-    def __init__(self, name, author, title, description, url, urlToImage,  publishedAt):
+    def __init__(self, name, description, url, country):
         self.name = name
-        self.author = author
-        self.title = title
         self.description = description
         self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
+        self.country = country
+        
