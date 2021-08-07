7d625046234a84e4e27483625d7a73f128b4826c
Francis Kinyae <fmkinyae@gmail.com>
2021-08-07 20:07:56 +0300

    add news categories

--- app/views.py
+++ app/views.py
@@ -9,7 +9,15 @@ def index():
     function that returns the index page and its data 
     '''
     #Get popular news
-    popular_news = get_news('popular')
-    print(popular_news)
-    title = 'Welcome to Your News Agent'
-    return render_template('index.html', title = title, popular = popular_news)
+    general_news = get_news('general')
+    sports_news = get_news('sports')
+    entertainment_news = get_news('entertainment')
+    health_news = get_news('health')
+    business_news = get_news('business')
+    tech_news = get_news('technology')
+    science_news = get_news('science')
+    
+    
+    
+    title = 'Your Trusted News Sources'
+    return render_template('index.html', title = title, general = general_news, sports = sports_news, entertainment = entertainment_news,   health = health_news, business = business_news, technology = tech_news, science = science_news)
