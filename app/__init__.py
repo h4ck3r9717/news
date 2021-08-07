5d78f509a3b86509aa969b4fdfb9cac720164595
Francis Kinyae <fmkinyae@gmail.com>
2021-08-07 10:45:31 +0300

    add new application configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+from app.config import DevConfig
 from flask import Flask
 
 #initialize the application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 
 from app import views
