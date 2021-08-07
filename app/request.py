0bafd2b99f6c96fa75c511bbc72f8f20c7f607f4
Francis Kinyae <fmkinyae@gmail.com>
2021-08-07 12:00:03 +0300

    add a request file for making thee api requests

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request, json
+from .models import news
+
+News = news.News
 
 #Get the api key
 api_key = app.config['NEWS_API_KEY']
+
+#Get news api url
+base_url = app.config['NEWS_API_BASE_URL']
