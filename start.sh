f36ac97d831a412d96836ebb121be3694612c10e
shawn <shawn@kali.Hacker>
2021-11-03 02:01:17 -0400

    initial

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-    export NEWS_API_KEY='77b20e99e33440d0a78e9a4d2ae613af'
+    export NEWS_API_KEY='a2d82a7064654a1cb1aae5f6fd53fede'
 export SECRET_KEY='885522'    
 
 python3.9  manage.py server
+y
